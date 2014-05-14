b100fdf701f088233f36ebccda57f682d3dfa856
Alina Sbirlea <alina@rice.edu>
2014-05-14 15:18:25 -0500

    Fix allocation.

--- examples/smithWaterman/fulltile.c
+++ examples/smithWaterman/fulltile.c
@@ -5,7 +5,7 @@ void fulltile( int i, int j, AItem A0p, AItem A1p, AItem A2p, Context *context){
     int (*A1)[TH] = (int(*)[TH])A1p.item;
     int (*A2)[TH] = (int(*)[TH])A2p.item;
 	int *A31D;
-	cncHandle_t A3_handle = cncCreateItem_A(&A31D, 1);
+	cncHandle_t A3_handle = cncCreateItem_A(&A31D, TH*TW);
 	int (*A3)[TH] = (int(*)[TH])A31D;
 
     int ii, jj;
