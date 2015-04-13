6ee6dfc8c43391e42ba4d177209274da13c9a990
Nick Vrvilo <Nick.Vrvilo@rice.edu>
2015-04-12 23:04:13 -0500

    Fixing header definitions for smith waterman example.

--- examples/smithWaterman/SmithWaterman_itemInfo.h
+++ examples/smithWaterman/SmithWaterman_itemInfo.h
@@ -1,15 +1,15 @@
+#ifndef _CNCOCR_SMITHWATERMAN_ITEM_INFO_H_
+#define _CNCOCR_SMITHWATERMAN_ITEM_INFO_H_
+
 #include "SmithWaterman.h"
 
+#define cncItemSanitizeFn_above NULL
 static inline int cncItemSize_above(SmithWatermanCtx *ctx) {
     return sizeof(int) * (ctx->tw+1);
 }
+#define cncItemSanitizeFn_left NULL
 static inline int cncItemSize_left(SmithWatermanCtx *ctx) {
     return sizeof(int) * (ctx->th+1);
 }
-static inline int cncItemSize_data(SmithWatermanCtx *ctx) {
-    return ctx->dataSize;
-}
-static inline int cncItemSize_startTime(SmithWatermanCtx *ctx) {
-    return sizeof(struct timeval);
-}
 
+#endif /*_CNCOCR_SMITHWATERMAN_ITEM_INFO_H_*/
